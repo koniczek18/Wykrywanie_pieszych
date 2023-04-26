b9f04dbada9926d9d10a53a161360c871793156c
koniczek <sz.zalas@gmail.com>
2023-04-26 23:28:59 +0200

    Display all 3 video captures TODO: Limit FPS of display

--- main.py
+++ main.py
@@ -2,32 +2,60 @@
 import numpy as np
 import cv2
 
-def nothing(x):
+def nothing(value):
     pass
 
-video = cv2.VideoCapture("xD.mp4")
-cv2.namedWindow("ddd")
-cv2.createTrackbar("LH", "ddd", 179, 179, nothing)
-cv2.createTrackbar("LS", "ddd", 255, 255, nothing)
-cv2.createTrackbar("LV", "ddd", 255, 255, nothing)
-cv2.createTrackbar("UH", "ddd", 179, 179, nothing)
-cv2.createTrackbar("US", "ddd", 255, 255, nothing)
-cv2.createTrackbar("UV", "ddd", 255, 255, nothing)
+videoPathThermal = "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_1.avi"
+videoPathNormal = "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_1.avi"
+videoPathMerged = "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_1.avi"
+
+videoT = cv2.VideoCapture(videoPathThermal)
+videoN = cv2.VideoCapture(videoPathNormal)
+videoM = cv2.VideoCapture(videoPathMerged)
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("LH", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("LS", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("LV", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("UH", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("US", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("UV", "Trackbars", 255, 255, nothing)
+
+dim = (640, 360)
+
+fpsT = videoT.get(cv2.CAP_PROP_FPS)
+fpsN = videoT.get(cv2.CAP_PROP_FPS)
+fpsM = videoT.get(cv2.CAP_PROP_FPS)
+print(fpsT)
+print(fpsN)
+print(fpsM)
 
 while True:
-    ret, frame = video.read()
+    retT, frameT = videoT.read()
+    retN, frameN = videoN.read()
+    retM, frameM = videoM.read()
 
-    if not ret:
-        video = cv2.VideoCapture("xD.mp4")
+    if (not retT) and (not retN):
+        videoT = cv2.VideoCapture(videoPathThermal)
+        retT, frameT = videoT.read()
+        videoN = cv2.VideoCapture(videoPathNormal)
+        retN, frameN = videoN.read()
+        videoM = cv2.VideoCapture(videoPathMerged)
+        retM, frameM = videoM.read()
         continue
 
-    LH = cv2.getTrackbarPos("LH", "ddd")
-    LS = cv2.getTrackbarPos("LS", "ddd")
-    LV = cv2.getTrackbarPos("LV", "ddd")
-    UH = cv2.getTrackbarPos("UH", "ddd")
-    US = cv2.getTrackbarPos("US", "ddd")
-    UV = cv2.getTrackbarPos("UV", "ddd")
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    LH = cv2.getTrackbarPos("LH", "Trackbars")
+    LS = cv2.getTrackbarPos("LS", "Trackbars")
+    LV = cv2.getTrackbarPos("LV", "Trackbars")
+    UH = cv2.getTrackbarPos("UH", "Trackbars")
+    US = cv2.getTrackbarPos("US", "Trackbars")
+    UV = cv2.getTrackbarPos("UV", "Trackbars")
+
+    frameT = cv2.resize(frameT, dim, interpolation=cv2.INTER_AREA)
+    frameN = cv2.resize(frameN, dim, interpolation=cv2.INTER_AREA)
+    frameM = cv2.resize(frameM, dim, interpolation=cv2.INTER_AREA)
+
+    hsv = cv2.cvtColor(frameT, cv2.COLOR_BGR2HSV)
     low = np.array([LH, LS, LV])
     high = np.array([UH, US, UV])
     mask = cv2.inRange(hsv, low, high)
@@ -37,12 +65,14 @@ while True:
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-    cv2.imshow("frame", frame)
-    cv2.imshow("mask", mask)
+            cv2.line(frameT, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    cv2.imshow("Thermal", frameT)
+    cv2.imshow("Normal", frameN)
+    cv2.imshow("Merged", frameM)
+    cv2.imshow("Mask Thermal", mask)
     key = cv2.waitKey(1)
     if key == 27:
         break
 
-video.release()
+videoT.release()
 cv2.destroyAllWindows()
