2a877544c35236641632dc9320df17e5a79b99d4
DarkningPl <76476752+DarkningPl@users.noreply.github.com>
2023-05-30 17:31:46 +0200

    Videos are now picked by changing vid_num, not the entire path Windows are moved accordingly to dim parameter

--- main.py
+++ main.py
@@ -7,14 +7,72 @@ def nothing(value):
     pass
 
 #file paths to video materials
-videoPathThermal = "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_1.avi"
-videoPathNormal = "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_1.avi"
-videoPathMerged = "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_1.avi"
+videoPathThermal = ["src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_1.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_3.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_4.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_5.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_6.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_7.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_8.avi",
+                    "src/video_in_motion/thermal/Jazda_LudzieNaTorach_Termo_10.avi",
+                    "src/video_in_motion/thermal/Jazda_ToryPuste_Termo_1.avi",
+                    "src/video_in_motion/thermal/Jazda_ToryPuste_Termo_2.avi",
+                    "src/video_in_motion/thermal/Jazda_ToryPuste_Termo_3.avi",
+                    "src/video_in_motion/thermal/Jazda_ToryWJezdni_Puste_Termo_1.avi",
+                    "src/video_in_motion/thermal/Jazda_ToryWJezdni_Samochod_Termo_1.avi",
+                    "src/video_in_motion/thermal/TorowiskoPuste_2_Termo.avi",
+                    "src/video_in_motion/thermal/TorowiskoPuste_3_Termo.avi",
+                    "src/video_stationary/thermal/Postoj_LudzieNaTorach_termo_1.avi",
+                    "src/video_stationary/thermal/Postoj_LudzieNaTorach_termo_3.avi",
+                    "src/video_stationary/thermal/Postoj_LudzieNaTorach_termo_7.avi",
+                    "src/video_stationary/thermal/Postoj_LudzieNaTorach_termo_10.avi",
+                    "src/video_stationary/thermal/Postoj_LudzieNaTorach_termo_12.avi"]
+videoPathNormal = ["src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_1.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_3.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_4.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_5.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_6.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_7.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_8.avi",
+                    "src/video_in_motion/normal/Jazda_LudzieNaTorach_Zwykla_10.avi",
+                    "src/video_in_motion/normal/Jazda_ToryPuste_Zwykla_1.avi",
+                    "src/video_in_motion/normal/Jazda_ToryPuste_Zwykla_2.avi",
+                    "src/video_in_motion/normal/Jazda_ToryPuste_Zwykla_3.avi",
+                    "src/video_in_motion/normal/Jazda_ToryWJezdni_Puste_Zwykla_1.avi",
+                    "src/video_in_motion/normal/Jazda_ToryWJezdni_Samochod_Zwykla_1.avi",
+                    "src/video_in_motion/normal/TorowiskoPuste_2_Zwykla.avi",
+                    "src/video_in_motion/normal/TorowiskoPuste_3_Zwykla.avi",
+                    "src/video_stationary/normal/Postoj_LudzieNaTorach_Zwykla_1.avi",
+                    "src/video_stationary/normal/Postoj_LudzieNaTorach_Zwykla_3.avi",
+                    "src/video_stationary/normal/Postoj_LudzieNaTorach_Zwykla_7.avi",
+                    "src/video_stationary/normal/Postoj_LudzieNaTorach_Zwykla_10.avi",
+                    "src/video_stationary/normal/Postoj_LudzieNaTorach_Zwykla_12.avi"]
+videoPathMerged = ["src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_1.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_3.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_4.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_5.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_6.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_7.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_8.avi",
+                    "src/video_in_motion/merged/Jazda_LudzieNaTorach_Merged_10.avi",
+                    "src/video_in_motion/merged/Jazda_ToryPuste_Merged_1.avi",
+                    "src/video_in_motion/merged/Jazda_ToryPuste_Merged_2.avi",
+                    "src/video_in_motion/merged/Jazda_ToryPuste_Merged_3.avi",
+                    "src/video_in_motion/merged/Jazda_ToryWJezdni_Puste_Merged_1.avi",
+                    "src/video_in_motion/merged/Jazda_ToryWJezdni_Samochod_Merged_1.avi",
+                    "src/video_in_motion/merged/TorowiskoPuste_2_Merged.avi",
+                    "src/video_in_motion/merged/TorowiskoPuste_3_Merged.avi",
+                    "src/video_stationary/merged/Postoj_LudzieNaTorach_Merged_1.avi",
+                    "src/video_stationary/merged/Postoj_LudzieNaTorach_Merged_3.avi",
+                    "src/video_stationary/merged/Postoj_LudzieNaTorach_Merged_7.avi",
+                    "src/video_stationary/merged/Postoj_LudzieNaTorach_Merged_10.avi",
+                    "src/video_stationary/merged/Postoj_LudzieNaTorach_Merged_12.avi"]
+vid_num = 0  # problems with 4, 15
 
 #initialise video capture based on file paths
-videoT = cv2.VideoCapture(videoPathThermal)
-videoN = cv2.VideoCapture(videoPathNormal)
-videoM = cv2.VideoCapture(videoPathMerged)
+videoT = cv2.VideoCapture(videoPathThermal[vid_num])
+videoN = cv2.VideoCapture(videoPathNormal[vid_num])
+videoM = cv2.VideoCapture(videoPathMerged[vid_num])
 
 #create windows
 cv2.namedWindow('Thermal')
@@ -24,13 +82,17 @@ cv2.namedWindow('Mask Thermal')
 cv2.namedWindow("Edges")
 cv2.namedWindow("Trackbars")
 
+#aditonal variables for resizing and fps limitation
+dim = (640, 360)
+fpsT = videoT.get(cv2.CAP_PROP_FPS)
+
 #move windows for convenience
 cv2.moveWindow('Thermal',0,0)
-cv2.moveWindow('Normal',640,0)
-cv2.moveWindow('Merged',1280,0)
-cv2.moveWindow('Edges',640,390)
-cv2.moveWindow('Mask Thermal',0,390)
-cv2.moveWindow('Trackbars',1280,390)
+cv2.moveWindow('Normal',dim[0],0)
+cv2.moveWindow('Merged',2*dim[0],0)
+cv2.moveWindow('Mask Thermal',0,30+dim[1])
+cv2.moveWindow('Edges',dim[0],30+dim[1])
+cv2.moveWindow('Trackbars',2*dim[0],30+dim[1])
 
 #initialise trackbars !!! THE FIRST VALUE IN TRACKBAR IS INITIAL, SECOND IS MAX!!!
 cv2.createTrackbar("LH", "Trackbars", 7, 179, nothing)
@@ -39,10 +101,6 @@ cv2.createTrackbar("LV", "Trackbars", 50, 255, nothing)
 cv2.createTrackbar("UH", "Trackbars", 173, 179, nothing)
 cv2.createTrackbar("US", "Trackbars", 7, 255, nothing)
 cv2.createTrackbar("UV", "Trackbars", 115, 255, nothing)
-
-#aditonal variables for resizing and fps limitation
-dim = (640, 360)
-fpsT = videoT.get(cv2.CAP_PROP_FPS)
 
 while True:
     #fps limiation
@@ -55,11 +113,11 @@ while True:
 
     #if video playback has ended, start it again
     if (not retT) and (not retN):
-        videoT = cv2.VideoCapture(videoPathThermal)
+        videoT = cv2.VideoCapture(videoPathThermal[vid_num])
         retT, frameT = videoT.read()
-        videoN = cv2.VideoCapture(videoPathNormal)
+        videoN = cv2.VideoCapture(videoPathNormal[vid_num])
         retN, frameN = videoN.read()
-        videoM = cv2.VideoCapture(videoPathMerged)
+        videoM = cv2.VideoCapture(videoPathMerged[vid_num])
         retM, frameM = videoM.read()
         continue
 
